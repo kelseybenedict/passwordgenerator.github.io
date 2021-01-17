846162f3566644bb2c3985f239706cfa6965a8ce
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-01-16 22:52:35 -0700

    added trim() to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var myArray = [];
   // Getting user input for the length of the password
-  var userLength = Number(prompt("How many characters would you like this password to be?"));
+  var userLength = Number(prompt("How many characters would you like this password to be? Please click 'OK' for yes, 'Cancel' for no").trim());
   // If the user selects "cancel" (or selects OK with a null value), ask them again
   if (!userLength){
     return writePassword();
@@ -21,26 +21,26 @@ function writePassword() {
     return writePassword();
   }
   // Getting user input to include numbers in password
-  var includeNumbers = confirm("Do you want this password to include numbers?");
+  var includeNumbers = confirm("Do you want this password to include numbers? Please click 'OK' for yes, 'Cancel' for no");
   if (includeNumbers){
     // If yes, store the numbers inside myArray
     myArray = myArray.concat(numbers);
     console.log("include numbers: ", myArray);
   }
   // Getting user input to include uppercase letters
-  var includeUpper = confirm("Do you want this password to include uppercase letters?");
-  // If yes, add uppercase letters to myArray
+  var includeUpper = confirm("Do you want this password to include uppercase letters? Please click 'OK' for yes, 'Cancel' for no");
+  // If yes, add uppercase letters to myArray 
   if (includeUpper){
     myArray = myArray.concat(upperCaseLetters);
     console.log("include uppers: ", myArray);
   }
-  var includeLower = confirm("Do you want this password to include lowercase letters?");
+  var includeLower = confirm("Do you want this password to include lowercase letters? Please click 'OK' for yes, 'Cancel' for no");
   if(includeLower){
     myArray = myArray.concat(lowerCaseLetters);
     console.log("include lowers: ", myArray);
   }
   // Getting user input to include special characters
-  var includeChar = confirm("Do you want this password to include special characters?");
+  var includeChar = confirm("Do you want this password to include special characters? Please click 'OK' for yes, 'Cancel' for no");
   if(includeChar){
     myArray = myArray.concat(specialChar);
     console.log("include special characters: ", myArray)
