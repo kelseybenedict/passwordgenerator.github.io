234d6311d807211d6ea66663160a73c93b13a43b
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-01-16 22:33:52 -0700

    added password logic to script

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,51 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
 // Assignment Code
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "/", ",", ".", "|", "~", "?"];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var myArray = [];
+  // Getting user input for the length of the password
+  var userLength = Number(prompt("How many characters would you like this password to be?"));
+  // If the user selects "cancel" (or selects OK with a null value), ask them again
+  if (!userLength){
+    return writePassword();
+  }
+  // Validating the user length
+  else if (userLength < 8 || userLength > 128){
+    alert("Please enter a valid number between 8 - 128");
+    // Start function over again
+    return writePassword();
+  }
+  // Getting user input to include numbers in password
+  var includeNumbers = confirm("Do you want this password to include numbers?");
+  if (includeNumbers){
+    // If yes, store the numbers inside myArray
+    myArray = myArray.concat(numbers);
+    console.log("include numbers: ", myArray);
+  }
+  // Getting user input to include uppercase letters
+  var includeUpper = confirm("Do you want this password to include uppercase letters?");
+  // If yes, add uppercase letters to myArray
+  if (includeUpper){
+    myArray = myArray.concat(upperCaseLetters);
+    console.log("include uppers: ", myArray);
+  }
+  var includeLower = confirm("Do you want this password to include lowercase letters?");
+  if(includeLower){
+    myArray = myArray.concat(lowerCaseLetters);
+    console.log("include lowers: ", myArray);
+  }
+  // Getting user input to include special characters
+  var includeChar = confirm("Do you want this password to include special characters?");
+  if(includeChar){
+    myArray = myArray.concat(specialChar);
+    console.log("include special characters: ", myArray)
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
